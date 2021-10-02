9730f4cfdeb03e395dbe22f08dcbeaf14e8c17db
_XXZZ00ZZXX_ <71839916+UserXTester@users.noreply.github.com>
2021-10-02 15:11:24 +0700

    Create Utils.py

---
+++ Utils.py
@@ -0,0 +1,17 @@
+RABSEN = [
+		"Hadir bosskuh! 😀",
+		"Hadir dev! 😃",
+		"Hadir ngab! 😄",
+		"Hadir kak!😁 ",
+		"Hadir broh! 😆",
+		"Hadir guru! 😂",
+		"Hadir mastah! 🤣",
+		"Hadir suhu! ☺️",
+		"Hadir brader 😊",
+		"Hadir brada 😇",
+		"Hadir man! 😉",
+		"Hadir master! 🤩",
+		"Hadir kawan! 😎",
+		"Hadir capt! 🥳",
+		"Hadir sir! 🤓"
+]
